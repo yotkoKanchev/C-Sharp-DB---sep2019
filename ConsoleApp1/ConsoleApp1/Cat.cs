c3980560705e65c386dff2f74d671ebf248cdac2
yotkoKanchev <yotko.kanchev@gmail.com>
2019-09-13 15:26:32 +0300

    test adding new project

---
+++ ConsoleApp1/ConsoleApp1/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Cat
+    {
+        public Cat (string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cat = new Cat("Tom");
+            Console.WriteLine(cat.Name);
+        }
+    }
+}
