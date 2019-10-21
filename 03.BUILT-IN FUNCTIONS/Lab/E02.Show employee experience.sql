838d4c3e3fa22997895cbe0abf4ab140155fb679
yotkoKanchev <yotko.kanchev@gmail.com>
2019-10-21 10:52:41 +0300

    Initial commit Entity Framework Core course oct2019

--- 03.BUILT-IN FUNCTIONS/Lab/E02.Show employee experience.sql
+++
@@ -1,8 +0,0 @@
-USE SoftUni
-
-GO
-
-SELECT EmployeeID,FirstName, LastName, 
-	   DATEDIFF(YEAR, HireDate, GETDATE())
-	AS [Years in Service]	
-  FROM Employees
--- 03.BUILT-IN FUNCTIONS/Lab/E04.Find all employees who's first name starts with Ro.sql
+++
@@ -1,7 +0,0 @@
-USE SoftUni
-
-GO
-
-SELECT EmployeeID, FirstName, LastName
-  FROM Employees
- WHERE FirstName LIKE 'Ro%'
