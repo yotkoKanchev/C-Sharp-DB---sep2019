4c22feac1b4f1c175fee4b25df87054826be4362
yotkoKanchev <yotko.kanchev@gmail.com>
2019-10-22 23:23:06 +0300

    updated solution

--- Entity Framework Core/01.DB APPS INTRODUCTION/Exercise/02.VillainNames/VillainNames.cs
+++ Entity Framework Core/01.DB APPS INTRODUCTION/Exercise/02.VillainNames/VillainNames.cs
@@ -15,12 +15,12 @@
 
             using (connection)
             {
-                var command = new SqlCommand("SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount " +
-                                           "    FROM Villains AS v " +
-                                           "    JOIN MinionsVillains AS mv ON v.Id = mv.VillainId " +
-                                           "GROUP BY v.Id, v.Name " +
-                                           "  HAVING COUNT(mv.VillainId) > 3 " +
-                                           "ORDER BY COUNT(mv.VillainId)", connection);
+                var command = new SqlCommand(@"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount 
+                                                FROM Villains AS v 
+                                                JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                            GROUP BY v.Id, v.Name 
+                                              HAVING COUNT(mv.VillainId) > 3 
+                                            ORDER BY COUNT(mv.VillainId)", connection);
                 var reader = command.ExecuteReader();
 
                 using (reader)
