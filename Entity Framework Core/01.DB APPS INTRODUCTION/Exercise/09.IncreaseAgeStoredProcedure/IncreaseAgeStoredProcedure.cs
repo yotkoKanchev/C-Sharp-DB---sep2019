287b0cdcb4c5a1e7b11570ce6998b417796e7d27
yotkoKanchev <yotko.kanchev@gmail.com>
2019-10-25 11:07:23 +0300

    Added P09 solution

---
+++ Entity Framework Core/01.DB APPS INTRODUCTION/Exercise/09.IncreaseAgeStoredProcedure/IncreaseAgeStoredProcedure.cs
@@ -0,0 +1,48 @@
+﻿namespace P09IncreaseAgeStoredProcedure
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public class IncreaseAgeStoredProcedure
+    {
+        public static void Main()
+        {
+            var connectionString = @"Server=YOTO\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
+
+            int minionId = int.Parse(Console.ReadLine());
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var query = @"EXECUTE dbo.usp_GetOlder @minionId";
+                var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@minionId", minionId);
+
+                using (command)
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                var printQuery = @"SELECT Name, Age 
+                                     FROM Minions 
+                                    WHERE Id = @minionId";
+
+                command.CommandText = printQuery;
+
+                var reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        var name = reader["Name"];
+                        var age = reader["Age"];
+                        Console.WriteLine($"{name} - {age} years old");
+                    }
+                }
+            }
+        }
+    }
+}
