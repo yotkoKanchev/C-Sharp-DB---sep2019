a767b2c8765754a1608a6c9ac860b34eb3e6ddd2
yotkoKanchev <yotko.kanchev@gmail.com>
2019-11-02 16:35:55 +0200

    Added P03 solution

---
+++ Entity Framework Core/04.CODE-FIRST/Exercise/3.SalesDatabase/Data/DataSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace P03_SalesDatabase.Data
+{
+    static class DataSettings
+    {
+        public const string DafaultConnection = @"Server=YOTO\SQLEXPRESS;Database=SalesDatabase;Integrated Security=True;";
+    }
+}
