63eb84dae215bab7b77484d2e35e96ae8b29cbe6
yotkoKanchev <yotko.kanchev@gmail.com>
2019-12-02 13:08:42 +0200

    Added EFC Exam 12.08.18 resources and ModelDefinition solution

---
+++ Entity Framework Core/88.OldExams/04.E_12.08.2018/SoftJail/Data/Models/Mail.cs
@@ -0,0 +1,22 @@
+﻿namespace SoftJail.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Mail
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Sender { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9\s]+ str.$"), Required]
+        public string Address { get; set; }
+
+        [Required]
+        public int PrisonerId { get; set; }
+        public Prisoner Prisoner { get; set; }
+    }
+}
---
+++ Entity Framework Core/88.OldExams/04.E_12.08.2018/SoftJail/Data/Models/Officer.cs
@@ -0,0 +1,29 @@
+﻿namespace SoftJail.Data.Models
+{
+    using SoftJail.Data.Models.Enums;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Officer
+    {
+        public int Id { get; set; }
+
+        [MinLength(3), MaxLength(30), Required]
+        public string FullName { get; set; }
+
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        public decimal Salary { get; set; }
+
+        [Required]
+        public Position Position { get; set; }
+
+        [Required]
+        public Weapon Weapon { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+
+        public ICollection<OfficerPrisoner> OfficerPrisoners { get; set; } = new HashSet<OfficerPrisoner>();
+    }
+}
