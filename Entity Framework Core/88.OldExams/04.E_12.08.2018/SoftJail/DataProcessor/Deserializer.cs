196228be85a9e87053a813fa08ae118f04edce60
yotkoKanchev <yotko.kanchev@gmail.com>
2019-12-02 14:02:50 +0200

    Added first import method OK

--- Entity Framework Core/88.OldExams/04.E_12.08.2018/SoftJail/DataProcessor/Deserializer.cs
+++ Entity Framework Core/88.OldExams/04.E_12.08.2018/SoftJail/DataProcessor/Deserializer.cs
@@ -1,24 +1,81 @@
 ﻿namespace SoftJail.DataProcessor
 {
 
-    using Data;
     using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using Data;
+    using Data.Models;
+    using DataProcessor.ImportDto;
+    using Newtonsoft.Json;
+    using System.Linq;
 
     public class Deserializer
     {
+        private const string ErrorMessage = "Invalid Data";
+
         public static string ImportDepartmentsCells(SoftJailDbContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
+            var departments = new List<Department>();
+            var importDepartmentDtos = JsonConvert.DeserializeObject<ImportDepartmentDto[]>(jsonString);
+
+            foreach (var departmentDto in importDepartmentDtos)
+            {
+                var cellsAreValid = departmentDto.Cells.All(c => IsValid(c));
+
+                if (!cellsAreValid || !IsValid(departmentDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                var cells = new List<Cell>();
+
+                foreach (var cellDto in departmentDto.Cells)
+                {
+                    cells.Add(new Cell
+                    {
+                        CellNumber = cellDto.CellNumber,
+                        HasWindow = cellDto.HasWindow,
+                    });
+                }
+
+                var department = new Department
+                {
+                    Name = departmentDto.Name,
+                    Cells = cells,
+                };
+
+                sb.AppendLine($"Imported {department.Name} with {department.Cells.Count} cells");
+                departments.Add(department);
+            }
+
+            context.Departments.AddRange(departments);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportPrisonersMails(SoftJailDbContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            return null;
+
         }
 
         public static string ImportOfficersPrisoners(SoftJailDbContext context, string xmlString)
         {
-            throw new NotImplementedException();
+            return null;
+
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
         }
     }
 }
---
+++ Entity Framework Core/88.OldExams/04.E_12.08.2018/SoftJail/DataProcessor/ImportDto/ImportDepartmentDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SoftJail.DataProcessor.ImportDto
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ImportDepartmentDto
+    {
+        [MinLength(3), MaxLength(25), Required]
+        public string Name { get; set; }
+
+        public ImportCellDto[] Cells { get; set; }
+    }
+}
