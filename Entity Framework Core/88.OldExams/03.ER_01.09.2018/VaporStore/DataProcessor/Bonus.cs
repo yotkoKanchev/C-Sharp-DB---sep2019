869993f7f3636d49be22fc34477b4ebee79e6125
yotkoKanchev <yotko.kanchev@gmail.com>
2019-12-03 10:25:52 +0200

    Added bonus method solution OK

--- Entity Framework Core/88.OldExams/03.ER_01.09.2018/VaporStore/DataProcessor/Bonus.cs
+++ Entity Framework Core/88.OldExams/03.ER_01.09.2018/VaporStore/DataProcessor/Bonus.cs
@@ -1,13 +1,31 @@
 ﻿namespace VaporStore.DataProcessor
 {
 	using System;
-	using Data;
+    using System.Linq;
+    using Data;
 
 	public static class Bonus
 	{
 		public static string UpdateEmail(VaporStoreDbContext context, string username, string newEmail)
 		{
-            return null;
+            var user = context.Users.FirstOrDefault(u => u.Username == username);
+
+            if (user == null)
+            {
+                return $"User {username} not found";
+            }
+
+            var email = context.Users.FirstOrDefault(u => u.Email == newEmail);
+
+            if (email != null)
+            {
+                return $"Email {newEmail} is already taken";
+            }
+
+            user.Email = newEmail;
+            context.SaveChanges();
+
+            return $"Changed {username}'s email successfully";
         }
 	}
 }
