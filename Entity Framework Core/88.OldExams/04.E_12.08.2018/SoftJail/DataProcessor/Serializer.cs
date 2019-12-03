4f9a81c92a0044293e60a665467f3b8c3a452ba4
yotkoKanchev <yotko.kanchev@gmail.com>
2019-12-03 09:31:15 +0200

    Added first export method solution OK

--- Entity Framework Core/88.OldExams/04.E_12.08.2018/SoftJail/DataProcessor/Serializer.cs
+++ Entity Framework Core/88.OldExams/04.E_12.08.2018/SoftJail/DataProcessor/Serializer.cs
@@ -2,14 +2,36 @@
 {
 
     using Data;
+    using Newtonsoft.Json;
     using System;
+    using System.Linq;
 
     public class Serializer
     {
         public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
         {
-            return null;
+            var prisoners = context.Prisoners
+                .Where(p => ids.Contains(p.Id))
+                .OrderBy(p => p.FullName)
+                .ThenBy(p => p.Id)
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    CellNumber = p.Cell.CellNumber,
+                    Officers = p.PrisonerOfficers
+                        .Select(o => new
+                        {
+                            OfficerName = o.Officer.FullName,
+                            Department = o.Officer.Department.Name,
+                        })
+                        .OrderBy(o => o.OfficerName)
+                        .ToList(),
+                    TotalOfficerSalary = Math.Round(p.PrisonerOfficers.Sum(po => po.Officer.Salary), 2)
+                })
+                .ToList();
 
+            return JsonConvert.SerializeObject(prisoners, Formatting.Indented);
         }
 
         public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
