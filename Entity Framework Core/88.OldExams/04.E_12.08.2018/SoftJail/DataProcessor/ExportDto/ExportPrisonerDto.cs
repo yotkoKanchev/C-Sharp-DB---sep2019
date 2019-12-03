25bc8a21d72ba13e5c2c0206234f9dd2daa7ff04
yotkoKanchev <yotko.kanchev@gmail.com>
2019-12-03 10:10:42 +0200

    Added second export method OK

---
+++ Entity Framework Core/88.OldExams/04.E_12.08.2018/SoftJail/DataProcessor/ExportDto/ExportPrisonerDto.cs
@@ -0,0 +1,20 @@
+﻿namespace SoftJail.DataProcessor.ExportDto
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Prisoner")]
+    public class ExportPrisonerDto
+    {
+        [XmlElement]
+        public int Id { get; set; }
+
+        [XmlElement]
+        public string Name { get; set; }
+
+        [XmlElement]
+        public string IncarcerationDate { get; set; }
+
+        [XmlArray]
+        public ExportMessageDto[] EncryptedMessages { get; set; }
+    }
+}
--- Entity Framework Core/88.OldExams/04.E_12.08.2018/SoftJail/DataProcessor/Serializer.cs
+++ Entity Framework Core/88.OldExams/04.E_12.08.2018/SoftJail/DataProcessor/Serializer.cs
@@ -3,8 +3,15 @@
 
     using Data;
     using Newtonsoft.Json;
+    using SoftJail.DataProcessor.ExportDto;
     using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
     using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
 
     public class Serializer
     {
@@ -31,13 +38,50 @@
                 })
                 .ToList();
 
-            return JsonConvert.SerializeObject(prisoners, Formatting.Indented);
+            return JsonConvert.SerializeObject(prisoners, Newtonsoft.Json.Formatting.Indented);
         }
 
         public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
         {
-            return null;
+            var names = prisonersNames.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            var prisoners = context.Prisoners
+                .Where(p => names.Contains(p.FullName))
+                .OrderBy(p => p.FullName)
+                .ThenBy(p => p.Id)
+                .Select(p => new ExportPrisonerDto
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    IncarcerationDate = p.IncarcerationDate.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture),
+                    EncryptedMessages = p.Mails.Select(m => new ExportMessageDto
+                    {
+                        Description =  ReverseString(m.Description)
+                    }).ToArray()
+                })
+                .ToArray();
 
+            var serializer = new XmlSerializer(typeof(ExportPrisonerDto[]), new XmlRootAttribute("Prisoners"));
+            var ns = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+            var sb = new StringBuilder();
+
+            using (var writer = new StringWriter(sb))
+            {
+                serializer.Serialize(writer, prisoners, ns);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static string ReverseString(string description)
+        {
+            var reversed = string.Empty;
+
+            for (int i = description.Length - 1; i >= 0; i--)
+            {
+                reversed += description[i];
+            }
+
+            return reversed;
         }
     }
 }
