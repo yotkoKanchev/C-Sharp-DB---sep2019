18be53120128279e99da90670a889aa264deebee
yotkoKanchev <yotko.kanchev@gmail.com>
2019-12-05 00:13:33 +0200

    Refactoring Deserializer

--- Entity Framework Core/88.OldExams/06.ER_05.01.2018/PetClinic/DataProcessor/Deserializer.cs
+++ Entity Framework Core/88.OldExams/06.ER_05.01.2018/PetClinic/DataProcessor/Deserializer.cs
@@ -4,14 +4,14 @@
     using System.Globalization;
     using System.Linq;
     using System.Text;
+    using System.IO;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    using Data;
-    using DataProcessor.ImportDtos;
-    using Models;
-    using Newtonsoft.Json;
     using System.Xml.Serialization;
-    using System.IO;
+    using Data;
+    using Models;
+    using DataProcessor.ImportDtos;
+    using Newtonsoft.Json;
 
     public class Deserializer
     {
@@ -135,63 +135,6 @@
 
         public static string ImportProcedures(PetClinicContext context, string xmlString)
         {
-            // ZERO TEST PASS OK but TEST4 doesn't pass :(
-
-            //var vets = context.Vets.ToList();
-            //var vetNames = vets.Select(v => v.Name).ToList();
-            //var animals = context.Animals.ToList();
-            //var animalNumbers = animals.Select(a => a.PassportSerialNumber).ToList();
-            //var animalAids = context.AnimalAids.ToList();
-            //var animalAidNames = animalAids.Select(aa => aa.Name).ToList();
-
-            //var procedures = new List<Procedure>();
-
-            //var sb = new StringBuilder();
-            //var serializer = new XmlSerializer(typeof(ImportProcedureDto[]), new XmlRootAttribute("Procedures"));
-
-            //ImportProcedureDto[] importProcedureDtos;
-
-            //using (var reader = new StringReader(xmlString))
-            //{
-            //    importProcedureDtos = (ImportProcedureDto[])serializer.Deserialize(reader);
-            //}
-
-            //foreach (var dto in importProcedureDtos)
-            //{
-            //    var animalAidsAreValid = dto.AnimalAids.All(aa => animalAidNames.Contains(aa.Name)) &&
-            //        dto.AnimalAids.Any() &&
-            //        dto.AnimalAids.Count() == dto.AnimalAids.Distinct().Count();
-
-            //    if (!IsValid(dto) ||
-            //        !vetNames.Contains(dto.Vet) ||
-            //        !animalNumbers.Contains(dto.Animal) ||
-            //        !animalAidsAreValid ||
-            //        procedures.Any(p => p.Animal.PassportSerialNumber == dto.Animal))
-            //    {
-            //        sb.AppendLine(ErrorMessage);
-            //        continue;
-            //    }
-
-            //    var procedure = new Procedure
-            //    {
-            //        Animal = animals.First(a => a.PassportSerialNumber == dto.Animal),
-            //        Vet = vets.First(v => v.Name == dto.Vet),
-            //        DateTime = DateTime.Parse(dto.DateTime),
-            //        ProcedureAnimalAids = dto.AnimalAids.Select(aa => new ProcedureAnimalAid
-            //        {
-            //            AnimalAid = animalAids.First(a => a.Name == aa.Name),
-            //        }).ToArray()
-            //    };
-
-            //    procedures.Add(procedure);
-            //    sb.AppendLine("Record successfully imported.");
-            //}
-
-            //context.Procedures.AddRange(procedures);
-            //context.SaveChanges();
-
-            //return sb.ToString().TrimEnd();
-
             var sb = new StringBuilder();
             var procedures = new List<Procedure>();
 
@@ -214,8 +157,11 @@
                 var animalIsValid = animals.Any(a => a.PassportSerialNumber == dto.Animal);
                 var animalAidsAreValid = dto.AnimalAids.All(aa => animalAids.Any(a => a.Name == aa.Name));
 
+                //check all animalAids are unique
                 var animalAidsNames = dto.AnimalAids.Select(aa => aa.Name).ToList();
                 var namesAreUnique = animalAidsNames.Count() == animalAidsNames.Distinct().ToList().Count();
+
+
                 if (!IsValid(dto) || !vetIsValid || !animalIsValid || !animalAidsAreValid || !namesAreUnique)
                 {
                     sb.AppendLine(ErrorMessage);
