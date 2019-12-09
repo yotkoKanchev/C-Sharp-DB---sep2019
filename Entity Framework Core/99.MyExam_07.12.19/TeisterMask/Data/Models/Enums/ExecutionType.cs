32ad53d957de3a9c477c4eb8a9bf7d7f29ffb629
yotkoKanchev <yotko.kanchev@gmail.com>
2019-12-09 10:13:23 +0200

    Updated enumerations

--- Entity Framework Core/99.MyExam_07.12.19/TeisterMask/Data/Models/Enums/ExecutionType.cs
+++ Entity Framework Core/99.MyExam_07.12.19/TeisterMask/Data/Models/Enums/ExecutionType.cs
@@ -2,9 +2,9 @@
 {
     public enum ExecutionType
     {
-        ProductBacklog = 1,
-        SprintBacklog = 2,
-        InProgress = 3,
-        Finished = 4,
+        ProductBacklog,
+        SprintBacklog,
+        InProgress,
+        Finished,
     }
 }
--- Entity Framework Core/99.MyExam_07.12.19/TeisterMask/Data/Models/Enums/LabelType.cs
+++ Entity Framework Core/99.MyExam_07.12.19/TeisterMask/Data/Models/Enums/LabelType.cs
@@ -2,10 +2,10 @@
 {
     public enum LabelType
     {
-        Priority = 1, 
-        CSharpAdvanced = 2, 
-        JavaAdvanced = 3, 
-        EntityFramework = 4, 
-        Hibernate = 5,
+        Priority,
+        CSharpAdvanced,
+        JavaAdvanced,
+        EntityFramework,
+        Hibernate,
     }
 }
